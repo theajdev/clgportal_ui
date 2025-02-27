60f019a232f7c45c432b1ec066be4fa1bb801110
Ajay Kamble <61040208+ajaikamble10@users.noreply.github.com>
2025-02-27 15:04:30 +0530

    AuthService changed.

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const AUTH_REST_API_BASE_URL = 'https://clgportal-api.onrender.com/api/auth';
-const AUTH_REST_API_BASE_URL = 'http://172.31.224.1:2025/api/auth'
+const AUTH_REST_API_BASE_URL = 'https://clgportal-api.onrender.com/api/auth';
+//const AUTH_REST_API_BASE_URL = 'http://172.31.224.1:2025/api/auth'
 export const loginAPICall = (usernameOrEmail, password) => axios.post(AUTH_REST_API_BASE_URL + '/login', { usernameOrEmail, password });
 export const storeToken = (token) => localStorage.setItem("token", token);
 export const getToken = (token) => localStorage.getItem("token");
